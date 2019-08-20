3f27ce6d80a736d92e7af065f87ba3f7f4f6f8d3
Rodrigo Chamma Castro <rodrigochamma@gmail.com>
2019-08-20 00:46:33 -0700

    setup webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
 const path = require('path')
 
-module.exports = {
-    //Basic configurations
-    entry: './src/app.js',
-    //entry: './src/playground/redux-expensify.js',
-    //entry: './src/playground/hoc.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    //Configuration for babel to process all js files excluding the node_modules directory
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/, 
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    //good tool for help on development.
-    devtool: 'cheap-module-eval-source-map',    
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-}
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        //Basic configurations
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        //Configuration for babel to process all js files excluding the node_modules directory
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/, 
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        //good tool for help on development.
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',    
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
+};
