2343bc1126ac0d37d1275e1dbef06d860693878b
Rodrigo Chamma Castro <rodrigochamma@gmail.com>
2019-08-20 17:41:34 -0700

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
         //Basic configurations
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         //Configuration for babel to process all js files excluding the node_modules directory
@@ -47,7 +47,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',    
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     };
 };
